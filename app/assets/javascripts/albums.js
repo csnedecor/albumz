2592f25d7c051f61e479496e0cb8fb27a4d76e36
Helen Hood <helen.c.hood@gmail.com>
2014-12-11 14:22:41 -0500

    Working image preview

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -18,9 +18,21 @@
 //   });
 // });
 
+
+//  This works if you manually append an input instead of using cocoon -
+// the event handlers are added to the new input tags properly:
+
+// $(function() {
+//   $('.nested-fields').on('change', '.pictureInput', function(event) {
+//     alert('clicked the nested fields box');
+//     $(this).parent().append('<input class="pictureInput" type="file" />')
+//   });
+// });
+
 $(function() {
+
+  // This doesn't work for additional elements insterted via the cocoon link:
   $('.nested-fields').on('change', '.pictureInput', function(event) {
-    alert("changed!");
     var files = event.target.files;
     var image = files[0]
     var reader = new FileReader();
@@ -32,5 +44,25 @@ $(function() {
     }
     reader.readAsDataURL(image);
     console.log(files);
+  });
+
+  // this adds the handler to newly-inserted inputs
+  $('body').on('cocoon:after-insert', function(event) {
+
+    // add handler to new element
+    $('.nested-fields').on('change', '.pictureInput', function(event) {
+      var files = event.target.files;
+      var image = files[0]
+      var reader = new FileReader();
+      reader.onload = function(file) {
+        var img = new Image();
+        console.log(file);
+        img.src = file.target.result;
+        // instead of replacing the HTML of the target, we need to append
+        $('#target').append(img);
+      }
+      reader.readAsDataURL(image);
+      console.log(files);
+    });
   });
 });
